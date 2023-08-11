38c94e1e2eb0c1d7f2e2e128eb702d3a852dd69f
mahdi83777 <mye09@mail.aub.edu>
2023-08-11 18:33:12 +0300

    auth.js, helpers, footer.js

---
+++ client/src/utils/helpers.js
@@ -0,0 +1,9 @@
+export function formatDate(timestamp) {
+    let date = new Date(timestamp / 1);
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let year = date.getFullYear();
+    let formattedDate = `${month}/${day}/${year}`;
+    return formattedDate;
+  }
+  
