10b6d01b6600fb64211d724695a2d593cd30d1b6
Mohammadmahdi Moradzadeh <126528880+Mahdi-Moradzadeh@users.noreply.github.com>
2023-08-16 00:40:41 -0400

    restyled homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,46 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+
 import logoImage from "../assets/images/budgetwise.png";
 import expenseimg from "../assets/images/expensetracking.jpg";
 import chartimg from "../assets/images/charts.jpg";
 import analyticsimg from "../assets/images/analytics.jpg";
 
 const HomePage = () => {
-
-  const [transaction, setTransaction] = useState("Income");
-  const handleTransactionTab = (event) => {
-    setTransaction(event.target.textContent);
-  };
-  useEffect(() => {
-    console.log(transaction);
-  }, [transaction]);
   return (
     <div className="homepage-container">
-      <section className="hero-section">
-      <img src={logoImage} class="mx-auto d-block"  alt="Budget Wise Logo" className="logo" height="800" width="800" />
+      <section className="hero-section text-center">
+        <img src={logoImage} alt="Budget Wise Logo" className="logo" />
       </section>
-      <div class="container text-center">    
-  <div class="container-fluid bg-3 text-center bg-red">    
-  <div class="row">
-    <div class="col-sm-4">
-    <h3>Expense Tracker</h3>
-    <p class="text-info">Effortlessly monitor your expenses and stay on top of your budget.</p>
-      {<img src={expenseimg} class="responsive" alt="Expense img example" className="logo" />}
+
+      <div className="container text-center features-section">    
+        <FeatureCard 
+          title="Expense Tracker" 
+          description="Effortlessly monitor your expenses and stay on top of your budget." 
+          imgSrc={expenseimg} 
+          altText="Expense img example"
+        />
+        <FeatureCard 
+          title="Analytics" 
+          description="Receive tips for recurring bill payments and learn how to budget." 
+          imgSrc={analyticsimg} 
+          altText="analytics img example"
+        />
+        <FeatureCard 
+          title="Data Visualization" 
+          description="Visualize your financial progress with intuitive graphs and charts." 
+          imgSrc={chartimg} 
+          altText="Chart example"
+        />
+      </div>
     </div>
-    <div class="col-sm-4"> 
-    <h3>Analytics</h3>
-    <p class="text-info">Receive tips for recurring bill payments and learn how to budget.</p>
-      {<img src={analyticsimg} class="responsive" alt="analytics img example" className="logo" />}
-    </div>
-    <div class="col-sm-4"> 
-    <h3>Data Visualization</h3>
-    <p class="text-info">Visualize your financial progress with intuitive graphs and charts.</p>
-      {<img src={chartimg} class="responsive" alt="Chart example" className="logo" />}
-    </div>
-  </div>
-</div>
-</div>
-</div>
   );
 };
+
+const FeatureCard = ({ title, description, imgSrc, altText }) => (
+  <div className="feature-card">
+    <img src={imgSrc} alt={altText} className="feature-img" />
+    <h3 className="feature-title">{title}</h3>
+    <p className="feature-description">{description}</p>
+  </div>
+);
+
 export default HomePage;
